e1cf7e7e0014fd54b207b9431cd6674d19015558
Francis Yang <fy50167@gmail.com>
2024-03-13 16:06:58 -0700

    finishing section skeleton

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,5 @@
+import SectionSvg from '../assets/svg/SevtionSvg';
+
 export default function Section({
     className,
     id,
@@ -18,6 +20,23 @@ export default function Section({
             } ${className || ''}`}
         >
             {children}
+            <div className='absolute hidden top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10'></div>
+            <div className='absolute hidden top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10'></div>
+            {crosses && (
+                <>
+                    <div
+                        className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+                            crossesOffset &&
+                            crossesOffset
+                        } pointer-events-none lg:block xl:left-10 right-10`}
+                    />
+                    <SectionSvg
+                        crossesOffset={
+                            crossesOffset
+                        }
+                    />
+                </>
+            )}
         </div>
     );
 }
