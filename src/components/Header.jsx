9c7ef823048dec858657a09d8d11510fb5fc4bbe
Francis Yang <fy50167@gmail.com>
2024-03-13 14:56:27 -0700

    creating header omponent

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { brainwave } from '../assets';
+
+export default function Header() {
+    return (
+        <div className='fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backrop-blur-sm'>
+            <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
+                <a className='block w-[12rem] xl:mr-8' href='#hero'>
+                    <img
+                        width={190}
+                        height={40}
+                        alt='Brainwave'
+                        src={brainwave}
+                    />
+                </a>
+            </div>
+        </div>
+    );
+}
