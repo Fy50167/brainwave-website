8f688b8eb4d485046224d8c892b9948e98e85f0e
Francis Yang <fy50167@gmail.com>
2024-03-13 15:12:37 -0700

    all anv styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { brainwave } from '../assets';
 import { navigation } from '../constants';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
+    const pathname = useLocation();
+
     return (
         <div className='fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backrop-blur-sm'>
             <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
@@ -25,6 +28,16 @@ export default function Header() {
                                     href={
                                         item.url
                                     }
+                                    className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                                        item.onlyMobile
+                                            ? 'lg:hidden'
+                                            : ''
+                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                                        item.url ===
+                                        pathname.hash
+                                            ? 'z-2 lg:text-n-1'
+                                            : 'lg:text-n-1/50'
+                                    } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
                                 >
                                     {item.title}
                                 </a>
