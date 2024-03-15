0d1f2e0da5479cefe159d4a68cd10ef18d51aa37
Francis Yang <fy50167@gmail.com>
2024-03-14 17:22:45 -0700

    adding initial header text for hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { curve } from '../assets';
+import Button from './Button';
 import Section from './Section';
 
 export default function Hero() {
@@ -8,6 +10,32 @@ export default function Hero() {
             crossesOffset='lg:translate-y-[5.25rem]'
             customPaddings
             id='hero'
-        ></Section>
+        >
+            <div className='container relative'>
+                <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]'>
+                    <h1 className='h1 mb-6'>
+                        Explore the Possibilities of AI Chatting with
+                        BrainwaveCurve
+                        <span className='inline-block relative'>
+                            Brainwave
+                            <img
+                                src={curve}
+                                className='absolute top-full left-0 w-full xl:-mt-2'
+                                width={624}
+                                height={28}
+                                alt='curve'
+                            />
+                        </span>
+                    </h1>
+                    <p className='body-1 max-w-3xl mx-auto text-n-2 mb-6 lg:mb-8'>
+                        Unleash the power of AI within Brainwave. Upgrade your
+                        productivity with Brainwave, the open AI chat app.
+                    </p>
+                    <Button href='/pricing' white>
+                        Get started
+                    </Button>
+                </div>
+            </div>
+        </Section>
     );
 }
