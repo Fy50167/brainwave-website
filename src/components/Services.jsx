42ca76e70ff25e8e1118179976bb5fc2aa78e3a1
Francis Yang <fy50167@gmail.com>
2024-03-15 07:21:34 -0700

    import design services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,14 @@
-import { check, service1 } from '../assets';
+import { check, service1, service2 } from '../assets';
 import { brainwaveServices } from '../constants';
 import Generating from './Generating';
 import Heading from './Heading';
 import Section from './Section';
+import {
+    PhotoChatMessage,
+    Gradient,
+    VideoBar,
+    VideoChatMessage,
+} from './design/Services';
 
 const Services = () => {
     return (
@@ -46,6 +52,28 @@ const Services = () => {
                             </ul>
                         </div>
                         <Generating className='absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2' />
+                    </div>
+
+                    <div className='relative z-1 grid gap-5 lg:grid-cols-2'>
+                        <div className='relative min-h-[39rem] border border-n-1/10 rounded-3xl oberflow-hidden'>
+                            <div className='absolute inset-0'>
+                                <img
+                                    src={service2}
+                                    className='w-full h-full object-cover'
+                                    width={630}
+                                    height={750}
+                                    alt='robot'
+                                />
+                            </div>
+                            <div className='absolute inset-0 flex flex-col justify-end -8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15'>
+                                <h4 className='h4 mb-4'>Photo editing</h4>
+                                <p className='body-2 mb-[3rem] text-n-3'>
+                                    Automatically enhance your photos using our
+                                    AI app's photo editing feature. Try it now!
+                                </p>
+                            </div>
+                            <PhotoChatMessage />
+                        </div>
                     </div>
                 </div>
             </div>
