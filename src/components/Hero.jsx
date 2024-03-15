f2e822b13ae0a1ca11808ea97c661a784f0a7575
Francis Yang <fy50167@gmail.com>
2024-03-14 18:12:20 -0700

    import notification component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,6 +69,12 @@ export default function Hero() {
                                         ))}
                                     </ul>
                                 </ScrollParallax>
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <Notification
+                                        className='hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex'
+                                        title='Code generation'
+                                    />
+                                </ScrollParallax>
                             </div>
                         </div>
                         <Gradient />
