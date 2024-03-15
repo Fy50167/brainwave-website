c25343ccb05808d1b271b32770431b84ed4cddcd
Francis Yang <fy50167@gmail.com>
2024-03-15 05:16:38 -0700

    display images

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Heading from './Heading';
 import { benefits } from '../constants';
+import Arrow from '../assets/svg/Arrow';
+import { GradientLight } from './design/Benefits';
+import ClipPath from '../assets/svg/ClipPath';
 
 const Benefits = () => {
     return (
@@ -23,7 +26,39 @@ const Benefits = () => {
                         <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
                             <h5 className='h5 mb-5'>{item.title}</h5>
                             <p className='body-2 mb-6 text-n-3'>{item.text}</p>
+                            <div className='flex items-center mt-auto'>
+                                <img
+                                    src={item.iconUrl}
+                                    idth={48}
+                                    heught={48}
+                                    alt={item.title}
+                                />
+                                <p className='ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider'>
+                                    Explore more
+                                </p>
+                                <Arrow />
+                            </div>
                         </div>
+
+                        {item.light && <GradientLight />}
+                        <div
+                            className='absolute inset-0.5 bg-n-8'
+                            style={{ clipPath: 'url(#benefits)' }}
+                        >
+                            <div className='absolute inset-0 opacity-0 hover:opacity-10 transition-opacity'>
+                                {item.imageUrl && (
+                                    <img
+                                        src={item.imageUrl}
+                                        width={380}
+                                        height={362}
+                                        alt={item.title}
+                                        className='w-full h-full object-cover'
+                                    />
+                                )}
+                            </div>
+                        </div>
+
+                        <ClipPath />
                     </div>
                 ))}
             </div>
