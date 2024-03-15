6ee186312bd98bbea8f901e23389fcda3c1566ef
Francis Yang <fy50167@gmail.com>
2024-03-14 17:17:45 -0700

    using section skeleton to start hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import Section from './Section';
+
+export default function Hero() {
+    return (
+        <Section
+            className='pt-[12rem] -mt-[5.25]'
+            crosses
+            crossesOffset='lg:translate-y-[5.25rem]'
+            customPaddings
+            id='hero'
+        ></Section>
+    );
+}
