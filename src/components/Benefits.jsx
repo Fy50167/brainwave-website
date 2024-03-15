6be89145381bc6ee71aac8fd70aecfb9f2efc577
Francis Yang <fy50167@gmail.com>
2024-03-15 05:10:28 -0700

    render benefits content

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import Heading from './Heading';
+import { benefits } from '../constants';
 
 const Benefits = () => {
     return (
         <Section id='features'>
             <div className='container relative z-2'>
-                <Heading />
+                <Heading
+                    className='md:max-w-md lg:max-w-2xl'
+                    title='Chat Smarter, Not Harder with Brainwave'
+                />
+            </div>
+            <div className='flex flex-wrap gap-10 mb-10'>
+                {benefits.map((item) => (
+                    <div
+                        className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]'
+                        key={item.id}
+                        style={{
+                            backgroundImage: `url(${item.backgroundUrl})`,
+                        }}
+                    >
+                        <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
+                            <h5 className='h5 mb-5'>{item.title}</h5>
+                            <p className='body-2 mb-6 text-n-3'>{item.text}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </Section>
     );
