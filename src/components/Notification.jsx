2eac7e6aef4f8274eb2e544a3c873008996b01b5
Francis Yang <fy50167@gmail.com>
2024-03-14 18:15:45 -0700

    work on notification component

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,7 +1,33 @@
-export default function Notification() {
+import { notificationImages } from '../constants';
+
+export default function Notification({ className, title }) {
     return (
-        <>
-            <div></div>
-        </>
+        <div
+            className={`${
+                className || ''
+            } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-10 rounded-2xl gap-5`}
+        >
+            <div className='flex-1'>
+                <h6 className='bg-1 font-semibold text-base'>{title}</h6>
+                <div classname='flex items-center justify-between'>
+                    <ul className='flex -m-0.5'>
+                        {notificationImages.map((item, index) => (
+                            <li
+                                key={index}
+                                className='flex w-6 border-2 border-n-12 h-6 rounded-full overflow-hidden'
+                            >
+                                <img
+                                    src={item}
+                                    className='ww-full'
+                                    width={20}
+                                    height={20}
+                                    alt='item'
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
