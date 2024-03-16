b6131e32a82f3151feef00c876db84e9f1c398ef
Francis Yang <fy50167@gmail.com>
2024-03-16 09:21:36 -0700

    changing main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
 import './index.css';
-import App from './App';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        errorElement: <Error />,
-        children: [],
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
