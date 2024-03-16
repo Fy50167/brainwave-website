f23aff60bdb0a12dd0d6e4b5229ac49f1e267dac
Francis Yang <fy50167@gmail.com>
2024-03-16 08:12:42 -0700

    render stars

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Section from './Section';
+import { smallSphere, stars } from '../assets';
 
 const Pricing = () => {
     return (
@@ -13,6 +14,15 @@ const Pricing = () => {
                         height={255}
                         alt='sphere'
                     />
+                    <div className='absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
+                        <img
+                            src={stars}
+                            className='w-full'
+                            width={950}
+                            height={400}
+                            alt='stars'
+                        />
+                    </div>
                 </div>
             </div>
         </Section>
